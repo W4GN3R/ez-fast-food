cb5acb95cc8a91b6e13f04feab3c5f3e0e6d8c11
Thaynara Silva <thaynara.silva@yaman.com.br>
2024-08-10 21:59:47 -0300

    correcao do database.sql que deu conflito

--- src/main/resources/database.sql
+++ src/main/resources/database.sql
@@ -1,23 +1,5 @@
 CREATE SCHEMA IF NOT EXISTS EZ_FASTFOOD;
 
--- Sera utilizado futuramente quando for habilitar a camada de seguranca (login com cpf e senha)
-/*CREATE TABLE IF NOT EXISTS EZ_FASTFOOD.CUSTOMERS(
-    id BIGSERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    cpf VARCHAR(14) UNIQUE,
-    email VARCHAR(255),
-    password VARCHAR (15)
-);
-
-INSERT INTO EZ_FASTFOOD.CUSTOMERS (name, cpf, email,password)
-VALUES 
-('Thaynara da Silva', '406.000.738-08', 'thaynara@example.com','senhA123'),
-('Flavio da Silva', '121.000.000-01', 'flavio@hotmail.com','fl@vio123'),
-('Wagner Cruz', '888.000.000-08', 'wagner@hotmail.com','w@gner123'),
-('Flavio Fernando da Silva', '000.000.000-00', 'fernando@hotmail.com','f$rnando123');
-
-*/
-
 
 CREATE TABLE IF NOT EXISTS EZ_FASTFOOD.CUSTOMER(
     id BIGSERIAL PRIMARY KEY,
@@ -26,19 +8,56 @@ CREATE TABLE IF NOT EXISTS EZ_FASTFOOD.CUSTOMER(
     email VARCHAR(255)
 );
 
-
-INSERT INTO EZ_FASTFOOD.CUSTOMER (name, cpf, email)
-VALUES 
-('Thaynara da Silva', '406.000.738-08', 'thaynara@example.com'),
-('Flavio da Silva', '121.000.000-01', 'flavio@hotmail.com'),
-('Wagner Cruz', '888.000.000-08', 'wagner@hotmail.com'),
-('Flavio Fernando da Silva', '000.000.000-00', 'fernando@hotmail.com');
-
-
 CREATE TABLE IF NOT EXISTS EZ_FASTFOOD.CATEGORY (
     id BIGSERIAL PRIMARY KEY,
     name VARCHAR(255)
 );
+
+CREATE TABLE IF NOT EXISTS EZ_FASTFOOD.PRODUCT (
+    id BIGSERIAL PRIMARY KEY,
+    category_id INT,
+    name VARCHAR(255),
+    description TEXT,
+    price DECIMAL,
+    FOREIGN KEY (category_id) REFERENCES EZ_FASTFOOD.CATEGORY(id)
+);
+
+CREATE TABLE IF NOT EXISTS EZ_FASTFOOD.ORDER (
+    id BIGSERIAL PRIMARY KEY,
+    customer_id INT NULL,
+    order_time TIMESTAMP WITH TIME ZONE,
+    total_price DECIMAL(10, 2),
+    order_status VARCHAR(50) NULL,
+    customer_name VARCHAR(255),
+    completed_time TIMESTAMP WITH TIME ZONE,
+    FOREIGN KEY (customer_id) REFERENCES EZ_FASTFOOD.CUSTOMER(id)
+);
+
+CREATE TABLE IF NOT EXISTS EZ_FASTFOOD.ORDERITEM (
+    id BIGSERIAL PRIMARY KEY,
+    order_id INT,
+    product_id INT,
+    quantity INT,
+    price DECIMAL,
+    FOREIGN KEY (order_id) REFERENCES EZ_FASTFOOD.ORDER(id),
+    FOREIGN KEY (product_id) REFERENCES EZ_FASTFOOD.PRODUCT(id)
+);
+
+CREATE TABLE IF NOT EXISTS EZ_FASTFOOD.PAYMENT (
+    id BIGSERIAL PRIMARY KEY,
+    order_id INT,
+    customer_id INT NULL,
+    payment_date DATE NULL,
+    payment_price DECIMAL,
+    payment_status VARCHAR(50),
+    FOREIGN KEY (order_id) REFERENCES EZ_FASTFOOD.ORDER(id),
+    FOREIGN KEY (customer_id) REFERENCES EZ_FASTFOOD.CUSTOMER(id)
+);
+
+INSERT INTO EZ_FASTFOOD.CUSTOMER (name, cpf, email)
+VALUES 
+('Thaynara da Silva', '359.380.170-17', 'thaynara@gmail.com'),
+('Flavio da Silva', '530.335.610-80', 'flavio@gmail.com');
 
 INSERT INTO EZ_FASTFOOD.CATEGORY (name)
 VALUES 
@@ -47,15 +66,6 @@ VALUES
 ('ACOMPANHAMENTO'),
 ('SOBREMESA');
 
-
-CREATE TABLE IF NOT EXISTS EZ_FASTFOOD.PRODUCT (
-    id BIGSERIAL PRIMARY KEY,
-    category_id INT,
-    name VARCHAR(255),
-    description TEXT,
-    unit_price DECIMAL,
-    FOREIGN KEY (category_id) REFERENCES CATEGORY(ID)
-);
 
 INSERT INTO EZ_FASTFOOD.PRODUCT (name,description,price, category_id)
 VALUES 
@@ -67,37 +77,4 @@ VALUES
 ('PETIT GATEAU', 'TIRA GOSTO','7.00',4);
 
 
-CREATE TABLE IF NOT EXISTS EZ_FASTFOOD.ORDER (
-    ID INT PRIMARY KEY,
-    customer_id INT NULL,
-    order_time TIME ZONE,
-    total_price DECIMAL(10, 2),
-    order_status VARCHAR(50) NULL,
-    customer_name VARCHAR(255),
-    completed_time TIME ZONE,
-    FOREIGN KEY (customer_id) REFERENCES CUSTOMER(ID)
-);
 
-CREATE TABLE IF NOT EXISTS EZ_FASTFOOD.ORDERITEM (
-    id BIGSERIAL PRIMARY KEY,
-    order_id INT,
-    product_id INT,
-    quantity INT,
-    price DECIMAL,
-    FOREIGN KEY (order_id) REFERENCES ORDER(ID),
-    FOREIGN KEY (product_id) REFERENCES PRODUCT(ID)
-);
-
-CREATE TABLE IF NOT EXISTS EZ_FASTFOOD.PAYMENT (
-    id BIGSERIAL PRIMARY KEY,
-    order_id INT,
-    customer_id INT NULL,
-    payment_date DATE NULL,
-    payment_price DECIMAL,
-    payment_status VARCHAR(50),
-    FOREIGN KEY (order_id) REFERENCES ORDER(ID),
-    FOREIGN KEY (customer_id) REFERENCES CUSTOMER(ID)
-);
-
---ALTER TABLE ez_fastfood."order" DROP CONSTRAINT IF EXISTS order_order_status_check;
---ALTER TABLE ez_fastfood."order" ADD CONSTRAINT order_order_status_check CHECK (order_status IN ('WAITING_PAYMENT', 'RECEIVED', 'IN_PREPARATION', 'READY', 'COMPLETED'));
